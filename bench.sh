458f816e701663892f7bc4d9ca0200ae8936b004
wxy <usstwxy@gmai.com>
2016-11-09 11:33:58 +0800

    add ffbench

--- bench.sh
+++ bench.sh
@@ -1,9 +1,12 @@
-# echo "nvidia gemm"
-# cd nvidia && make clean && make gemm && ./gemm < gemm.in && cd ..
+echo "nvidia gemm"
+cd nvidia && make clean && make gemm && ./gemm < gemm.in && cd ..
 
-# echo "intel gemm"
-# cd intel && make clean && make gemm && ./gemm < gemm.in && cd ..
+echo "intel gemm"
+cd intel && make clean && make gemm && ./gemm < gemm.in && cd ..
 
 echo "common memory"
-cd common && make clean && make memory && ./memory < memory.in && cd ..
+cd common/memory && make clean && make memory && ./memory < memory.in && cd ../..
+
+echo "ffbench"
+cd common/ffbench && make clean && make && time ./ffbench && cd ../..
 
